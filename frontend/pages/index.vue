1ee3e16acb47029225afca6a3f9fd474536f774d
David Ramirez <darksoulwk@gmail.com>
2024-10-30 11:51:32 -0500

    Add Array and v-for

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -4,35 +4,42 @@ definePageMeta({
   layout: "collapse",
   // middleware: 'auth',
 });
+
+const statics = [
+  {
+    name: "Total Companies",
+    number: 10,
+  },
+  {
+    name: "Total Tickets",
+    number: 20,
+  },
+  {
+    name: "Team Pending",
+    number: 35,
+  },
+  {
+    name: "In progress",
+    number: 5,
+  },
+  {
+    name: "Client Pending",
+    number: 70,
+  },
+  {
+    name: "Completed",
+    number: 300,
+  },
+];
 </script>
 
 <template>
   <div>
     Support Tickets
     <div class="grid grid-cols-6 gap-4">
-      <BaseCard class="p-4">
-        <span class="text-6xl"> 10 </span>
-        <p>Total Companies</p>
-      </BaseCard>
-      <BaseCard class="p-4">
-        <span class="text-6xl"> 20 </span>
-        <p>Total Tickets</p>
-      </BaseCard>
-      <BaseCard class="p-4">
-        <span class="text-6xl"> 35 </span>
-        <p>Team Pending</p>
-      </BaseCard>
-      <BaseCard class="p-4">
-        <span class="text-6xl"> 05 </span>
-        <p>In progress</p>
-      </BaseCard>
-      <BaseCard class="p-4">
-        <span class="text-6xl"> 70 </span>
-        <p>Client Pending</p>
-      </BaseCard>
-      <BaseCard class="p-4">
-        <span class="text-6xl"> 300 </span>
-        <p>Completed</p>
+      <BaseCard class="p-4" v-for="(item, index) in statics" :key="index">
+        <span class="text-6xl"> {{ item.number }} </span>
+        <p>{{ item.name }}</p>
       </BaseCard>
     </div>
     <!-- Padre -->
